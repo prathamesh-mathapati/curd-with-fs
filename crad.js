dd745e2c75cb0bbd24679ead1a65c4c560b67ebf
Prathamesh Mathapati <93044026+prathamesh-mathapati@users.noreply.github.com>
2021-11-11 19:49:11 +0530

    crad
    in this project i have written crad. This like create, update, delete Using fs module and store data in json.

---
+++ crad.js
@@ -0,0 +1,61 @@
+const fs=require('fs')
+
+try{
+    var a=fs.readFileSync('crad.json','utf-8')
+}catch(err){
+    var a=fs.writeFileSync('crad.json',JSON.stringify([]))
+}
+// creat
+function creat(){
+var a=require('prompt-sync')(),dic={}
+var a2=JSON.parse(fs.readFileSync('crad.json','utf-8'))
+dic["name"]=a('enter your name:-'),dic["id"]=a('enter your id:-'),dic["mob"]=a('enter your MO.NO:-')
+a2.push(dic)
+fs.writeFileSync('crad.json',JSON.stringify(a2))}
+
+//appadet
+function apppade_(){
+    var read=JSON.parse(fs.readFileSync('crad.json','utf-8'))
+    let b2=(require('prompt-sync')())
+    let b3=b2('enter id name:-')
+    for (i of read){
+        if (i['id']===b3){
+            let dict={},name1=b2('enter your name:-'),MO=b2('enter your MO.NO:-')
+            dict['name']=name1,dict['id']=i['id'],dict['MO.no']=MO
+            read.shift(i)
+            read.push(dict)
+            fs.writeFileSync('crad.json',JSON.stringify(read))
+        }
+    }
+}
+
+//delete
+function delet(){
+    var read=JSON.parse(fs.readFileSync('crad.json','utf-8'))
+    let b2=(require('prompt-sync')())
+    let b3=b2('enter id name:-')
+    for (i of read){
+        if (i['id']===b3){
+            read.shift(i)
+            fs.writeFileSync('crad.json',JSON.stringify(read))
+        }
+    }
+}
+let s=require('prompt-sync')()
+console.log('1. creat the new file\n2. upadet your data\n3. delete your data\n4. Out of crud opration');
+let option=s('select your option:-')
+if (option==1 || option==2 || option==3){
+    // while(true){
+        if (option==1){
+            creat()
+        }else if (option==2){
+            apppade_()
+        }else if(option==3){
+            delet()
+        // }
+
+    }
+}else {
+    console.log('you are out of crud opration');
+}
+  
